445269d4cb40f2d873f7d53a01781d76e7f008e7
BrandWest <brand.jwest@gmail.com>
2024-01-21 15:30:05 -0500

    Pointing to readme file

--- .github/scripts/update_table.sh
+++ .github/scripts/update_table.sh
@@ -12,7 +12,7 @@ COMMITS_TODAY=$(git log --since="today" --format="%H" | wc -l)
 # Get details of the last commit
 LAST_COMMIT_HASH=$(git log --format="%H" -n 1)
 LAST_COMMIT_MESSAGE=$(git log --format="%s" -n 1)
-sed -i -E "s/\| ([0-9]{4}-[0-9]{2}-[0-9]{2}) \| ([0-9]+) \| [a-f0-9]{7} - (.*)/| $TODAY | $COMMITS_TODAY | ${LAST_COMMIT_HASH:0:7} - $LAST_COMMIT_MESSAGE |/" ".github/workflows/test.md" 
+sed -i -E "s/\| ([0-9]{4}-[0-9]{2}-[0-9]{2}) \| ([0-9]+) \| [a-f0-9]{7} - (.*)/| $TODAY | $COMMITS_TODAY | ${LAST_COMMIT_HASH:0:7} - $LAST_COMMIT_MESSAGE |/" "README.md" 
 
 # # git add "$CURRENT_DIR/README.md"
 # # git commit -m "Update commit table with today's date and last commit details [skip ci]"
