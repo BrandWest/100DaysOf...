287b522b03e0f690f9cb2032c93881cde746755b
BrandWest <brand.jwest@gmail.com>
2024-01-21 15:28:56 -0500

    First action attempt

---
+++ .github/scripts/update_table.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the current directory (assuming the script is in .github/scripts)
+CURRENT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Get today's date
+TODAY=$(date +"%Y-%m-%d")
+
+# Get the current number of commits for today
+COMMITS_TODAY=$(git log --since="today" --format="%H" | wc -l)
+
+# Get details of the last commit
+LAST_COMMIT_HASH=$(git log --format="%H" -n 1)
+LAST_COMMIT_MESSAGE=$(git log --format="%s" -n 1)
+sed -i -E "s/\| ([0-9]{4}-[0-9]{2}-[0-9]{2}) \| ([0-9]+) \| [a-f0-9]{7} - (.*)/| $TODAY | $COMMITS_TODAY | ${LAST_COMMIT_HASH:0:7} - $LAST_COMMIT_MESSAGE |/" ".github/workflows/test.md" 
+
+# # git add "$CURRENT_DIR/README.md"
+# # git commit -m "Update commit table with today's date and last commit details [skip ci]"
+# # git push
+
