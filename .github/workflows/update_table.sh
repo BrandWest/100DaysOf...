ae094fa2bac72bfca35d3aa40e226f495f264c4a
BrandWest <brand.jwest@gmail.com>
2024-01-21 20:20:53 -0500

    Fixed tracking of commits in a day

--- .github/workflows/update_table.sh
+++ .github/workflows/update_table.sh
@@ -5,8 +5,11 @@ CURRENT_DIR=$(pwd)
 # Get today's date
 TODAY=$(date +"%Y-%m-%d")
 
-# Get the current number of commits for today
-COMMITS_TODAY=$(git log --since="today" --format="%H" | wc -l)
+#Get the arguement to run on a specific feature
+SEARCH_KEY=$1
+echo $SEARCH_KEY
+COMMITS_TODAY=$(git log --since="yesterday" --pretty=format:"%H" | while read -r COMMIT_HASH; do echo -n "Commit Hash: $COMMIT_HASH, Modified Top-level Folder: "; MODIFIED_FOLDER=$(git diff-tree --no-commit-id --name-only -r "$COMMIT_HASH" | awk -F/ '{print $1}'); echo "$MODIFIED_FOLDER"; done | uniq | grep -i $SEARCH_KEY | wc -l)
+
 
 # Get details of the last commit
 LAST_COMMIT_HASH=$(git log --format="%H" -n 1)
@@ -16,8 +19,7 @@ LAST_COMMIT_MESSAGE=$(git log --format="%s" -n 1)
 echo "Current Directory: $CURRENT_DIR"
 echo "Today's Date: $TODAY"
 echo "Last Commit Message: ${LAST_COMMIT_HASH:0:7} $LAST_COMMIT_MESSAGE"
-
-COMMITS_TODAY=$(git log --since="yesterday" --pretty=format:"%H" | while read -r COMMIT_HASH; do echo -n "Commit Hash: $COMMIT_HASH, Modified Top-level Folder: "; MODIFIED_FOLDER=$(git diff-tree --no-commit-id --name-only -r "$COMMIT_HASH" | awk -F/ '{print $1}'); echo "$MODIFIED_FOLDER"; done | uniq | grep -i prog | wc -l)
+echo "Total Commits: $COMMITS_TODAY"
 
 #Actual work
 sed -i -E "s/\| ([0-9]{4}-[0-9]{2}-[0-9]{2}) \| ([0-9]+) \| [a-f0-9]{7} - (.*)/| $TODAY | $COMMITS_TODAY | ${LAST_COMMIT_HASH:0:7} - $LAST_COMMIT_MESSAGE |/" "$CURRENT_DIR/README.md" 
